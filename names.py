c7837d38ab3768562ae6fb3254e618cdfe876f7d
Miguel Moura <miguelmouralob@gmail.com>
2024-03-05 14:04:42 -0300

    bd commit

--- names.py
+++ names.py
@@ -1,7 +1,27 @@
+import sqlite3
+
+connect = sqlite3.connect('dados_pessoas.db')
+cursor = connect.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS pessoas(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        idade INTEGER
+    )
+''')
+connect.commit()
+
+
+
+
 class Nomes():
     def __init__(self):
-        self.n = input('Digite seu nome: ').title()
-        self.id = int(input('Digite sua idade: '))
+        self.n = input('Digite o nome do usuário: ').title()
+        self.idade = int(input('Digite a idade: '))
+        cursor.execute('INSERT INTO pessoas (nome, idade) VALUES (?, ?)', (self.n, self.idade))
+        connect.commit()
+        self.id = cursor.lastrowid
 
 class Pw():
     def __init__(self):
@@ -15,7 +35,7 @@ pw = -1
 
 while pw != 1:
     pessoa = Nomes()
-    names.append(f'[{pessoa.n} ({pessoa.id})]')
+    names.append(f'[{pessoa.id} - {pessoa.n} ({pessoa.idade})]')
     print(', '.join(names))
 
     kp = -1
@@ -35,7 +55,9 @@ while pw != 1:
                 print('Digite uma opção válida!')
 
     if pessoa.id > maioridade:
-        maioridade = pessoa.id
+        maioridade = pessoa.idade
         maiornome = pessoa.n
+
+connect.close()
 
 print(f'{maiornome} é a pessoa mais velha do grupo com {maioridade} anos. ')
