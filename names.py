3e90e9e1ec4bf3025dd3a5e804b004a13f080044
Miguel Moura <miguelmouralob@gmail.com>
2024-03-05 21:37:37 -0300

    add sexuality

--- names.py
+++ names.py
@@ -8,6 +8,7 @@ cursor.execute('''
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome TEXT,
         idade INTEGER,
+        sexualidade TEXT,
         email TEXT
     )
 ''')
@@ -16,15 +17,34 @@ connect.commit()
 class Nomes():
     def __init__(self): 
         self.n = input('Digite o nome do usuário: ').title()
-        self.idade = int(input('Digite a idade: '))
+
+        while True:
+            self.idade = int(input('Digite a idade: '))
+            if self.idade >= 18 and self.idade <= 70:
+                break
+            else:
+                print('Apenas pesseoas entre 18-70 anos são aceitas!')
+
+        while True:
+            self.sexo = input('Digite a sexualidade (Masculino(M) OU Feminino(F)): ').upper()
+            if self.sexo.startswith('M') or self.sexo.startswith('F'):
+                break
+            else:
+                print('É preciso escolher o genêro entre Masculino(M) e Feminino(F)!')
+
         self.email = f'{self.n.lower()}{self.idade}@gmail.com'
-        cursor.execute('INSERT INTO pessoas (nome, idade, email) VALUES (?, ?, ?)', (self.n, self.idade, self.email))
+        cursor.execute('INSERT INTO pessoas (nome, idade, sexualidade, email) VALUES (?, ?, ?, ?)', (self.n, self.idade, self.sexo, self.email))
         connect.commit()
         self.id = cursor.lastrowid 
 
 class Pw():
     def __init__(self):
         self.pw = int(input('Quer continuar? Sim(0) ou Não(1): '))
+
+
+#class Sex():
+#    if pessoa.sexo == 
+
 
 names = []
 maioridade = 0
@@ -34,7 +54,7 @@ pw = -1
 
 while pw != 1:
     pessoa = Nomes()
-    names.append(f'[{pessoa.id} - {pessoa.n} ({pessoa.idade}), {pessoa.email}]')
+    names.append(f'[{pessoa.id} - {pessoa.n} ({pessoa.idade}), {pessoa.sexo}, {pessoa.email}]')
     print(', '.join(names))
 
     kp = -1
